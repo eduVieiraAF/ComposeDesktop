c26008d542efdd0149b402bc50349076a1a447ab
Eduardo Vieira <edu_vieira@hotmail.com>
2023-01-19 18:07:15 -0300

    Click events improvements

--- src/jvmMain/kotlin/examples/Example04.kt
+++ src/jvmMain/kotlin/examples/Example04.kt
@@ -3,16 +3,15 @@ package examples
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -23,7 +22,8 @@ fun exampl04() {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .padding(22.dp),
     ) {
         var text by remember { mutableStateOf("Click cyan box") }
 
@@ -31,14 +31,23 @@ fun exampl04() {
             Box(
                 modifier = Modifier
                     .fillMaxWidth(0.7f)
-                    .fillMaxHeight(0.2f)
+                    .fillMaxHeight(0.5f)
                     .background(Color.Cyan)
                     .combinedClickable(
                         onClick = { text = "Click! ${count++} " },
                         onDoubleClick = { text = "Double click! ${count++}" },
                         onLongClick = { text = "Long click! ${count++}" }
                     )
-            ) { Text(modifier = Modifier.fillMaxWidth(), text = text, textAlign = TextAlign.Center) }
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Center),
+                    text = text,
+                    textAlign = TextAlign.Center,
+                    fontSize = 22.sp
+                )
+            }
         }
     }
 }
