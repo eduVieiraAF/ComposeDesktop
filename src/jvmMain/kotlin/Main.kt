29241444e08eb0e3f641a73ecea4f58207c531b0
Eduardo Vieira <edu_vieira@hotmail.com>
2023-01-30 10:21:45 -0300

    Playing around

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,16 +1,21 @@
 import androidx.compose.runtime.key
 import androidx.compose.runtime.remember
+import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.singleWindowApplication
 import examples.MyApplicationState
 import examples.MyWindow
+import examples.exampl04
 
 
-fun main() = application {
-    val applicationState = remember { MyApplicationState() }
+fun main() = singleWindowApplication{
+   /* val applicationState = remember { MyApplicationState() }
 
     for (window in applicationState.windows) {
         key(window) {
             MyWindow(window)
         }
-    }
+    } */
+
+    exampl04()
 }
